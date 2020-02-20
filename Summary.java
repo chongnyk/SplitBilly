9452d3e68b0692db3a89dd30f5b30843efcc668e
GO48 <55522603+GO48@users.noreply.github.com>
2020-02-20 10:58:11 -0500

    Update Summary.java

--- Summary.java
+++ Summary.java
@@ -6,23 +6,44 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.util.Date;
 
 public class Summary extends AppCompatActivity
 {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_summary);
+        setContentView(R.layout.summary_layout);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openEntry();
+
+            }
+        });
     }
+
+    public void openEntry()
+    {
+        Intent intent = new Intent(this, Entry.class);
+        startActivity(intent);
+    }
+
     public void summaryButton(View view)
     {
         TextView summary = (TextView)findViewById(R.id.summary);
-        String result = "Billy has paid " + 1000 + " dollars until " + java.time.LocalDate.now();
-        result +="\n\n" + "Everyone owns Billy " + 250 + " dollars";
+        TextView totalTax = (TextView)findViewById(R.id.totalTax);
+        TextView totalSum = (TextView)findViewById(R.id.totalSum);
+        String sumAmount = totalSum.getText().toString();
+        double index = Double.parseDouble(sumAmount);
+        String result = "Billy has paid " +   "$ until " + java.time.LocalDate.now();
+        result +="\n\n" + "Everyone owns Billy " +   "$";
         summary.setText(result);
     }
 
